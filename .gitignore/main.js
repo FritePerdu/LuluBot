0434a223952bfbf77f29ef29d74feec43bba9c14
FritePerdu <35865192+FritePerdu@users.noreply.github.com>
2020-11-26 14:28:45 +0100

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -1,21 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-
 client.login(process.env.TOKEN);
-
-//Toutes les actions à faire quand le bot se connecte
 var prefix = ("-");
-
 client.on("message", (message) => {
-    
     if(message.content === "bonjour") {
-        message.channel.send("Salutation!")
+       message.channel.send("Salutation!")
     }
-    
     if(message.content === prefix + "help"){
        message.channel.send("Veux tu de l'aide ?")
     }
-    
-});
-
+})

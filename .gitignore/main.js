60a4d20c2016274164edae0c735d1761465aa301
FritePerdu <35865192+FritePerdu@users.noreply.github.com>
2020-11-26 13:46:12 +0100

    Create main.js

---
+++ .gitignore/main.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+//Toutes les actions à faire quand le bot se connecte
+client.on("ready", function () {
+    console.log("Mon BOT est Connecté");
+})
+
+client.login(process.env.TOKEN);
